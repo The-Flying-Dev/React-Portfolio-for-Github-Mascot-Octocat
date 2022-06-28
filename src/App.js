70583ad6db35e8a283d8c9aef4914f9835fc62a2
work <leandranm@outlook.com>
2022-06-28 01:55:27 -0700

    Router, useEffect, Api, Endpoint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Profile from './pages/Profile';
+import Projects from './pages/Projects';
+import logo from './assets/logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header logo={logo} />      
+        <Routes>
+          <Route path='/' element={<Profile userName='octocat'/>} />
+          <Route path='/projects' element={<Projects userName='octocat'/>} />
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
